fbc2554bbec75f1cbea864207487e28da7c22167
unknown <mahsa.abootalebi@gmail.com>
2021-05-07 03:23:06 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Layout from './Components/Layout/Layout'
 import BurgerBuilder from './Container/burderBuilder/BurgerBuilder'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Layout>
-      <BurgerBuilder></BurgerBuilder>
-    </Layout>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/" component={BurgerBuilder}></Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
